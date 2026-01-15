07e216d5979ef77bb517f8d517ed2676ae06bc50
hcaroli381 <hcaroli381@g.educaand.es>
2026-01-15 20:19:36 +0100

    Pruebas con JAvaScript, intento de comienzo

--- app.js
+++ app.js
@@ -1,17 +1,30 @@
-const nombre = document.querySelector("#nombre");
-const apellidos = document.querySelector("#apellidos");
-const edad = document.querySelector("#edad");
-const email = document.querySelector("#email");
-const password = document.querySelector("#password");
-const password2 = document.querySelector("#password2");
+const nombre = document.querySelector("#nombre").value.trim();
+const apellidos = document.querySelector("#apellidos").value.trim();
+const edad = document.querySelector("#edad").value.trim();
+const email = document.querySelector("#email").value.trim();
+const password = document.querySelector("#password").value.trim();
+const password2 = document.querySelector("#password2").value.trim();
 const formulario = document.querySelector("#formulario");
 const mensajes = document.querySelector("#mensajes");
+mensajes.className = "error";
 
-let hayError = 0;
+let errores = [];
 
-formulario.addEventListener("submit",function (event){
-    
+formulario.addEventListener("submit", function (event) {
+    event.preventDefault();
 });
+
+function validacionNombreApellido(nombreParameter) {
+    if (nombreParameter.trim().length < 3) {
+        errores.push("El nombre debe tener más de 3 caracteres.")
+    };
+
+};
+
+
+function validacionNombre = (validacionNombreApellido(nombre));
+
+
 
 let nombre1 = nombre.textContent;
 nombre1 = nombre1.trim();
