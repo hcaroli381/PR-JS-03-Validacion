1a4d5705690bef604aad797cf94cf9034ac9120a
hcaroli381 <hcaroli381@g.educaand.es>
2026-01-15 14:38:55 +0100

    Añadidas constantes

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 const nombre = document.querySelector("#nombre");
+const apellidos = document.querySelector("#apellidos");
+const edad = document.querySelector("#edad");
+const email = document.querySelector("#email");
+const password = document.querySelector("#password");
+const password2 = document.querySelector("#password2");
+const formulario = document.querySelector("#formulario");
+const mensajes = document.querySelector("#mensajes");
+
+let hayError = 0;
+
+formulario.addEventListener("submit",function (event){
+    
+});
+
+let nombre1 = nombre.textContent;
+nombre1 = nombre1.trim();
